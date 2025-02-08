effe83682f0441c0c172b2090ca0e7cc591ad8b1
zyaadhesham <zyaadhesham429@gmail.com>
2025-02-08 22:33:49 +0200

    edit

--- C_Language/C_Function/Ex_Find_Name.c
+++ C_Language/C_Function/Ex_Find_Name.c
@@ -1,30 +1,30 @@
 #include<stdio.h>
 #include<string.h>
 
+int findName(char Names[][14], int n, char name[]);
+
+void main(){
+
+char names[5][14] ={"Alaa","Osama","Mamdouh","Samy","Hossain"};
+char name[14];
+
+puts("Enter your first name: ");
+gets(name);
+
+if(findName(names,5,name))
+puts("Welcome");
+else
+puts("Goodby");
+}
+
 int findName(char Names[][14], int n, char name[])
 {
 int i;
 
 for(i=0;i<n;i++)
 {
-    if(strcmp(name,Names[i])==0)
-    {
+    if(stricmp(name,Names[i])==0)
         return 1;
-    }
 }
-
 return 0;
-}
-void main(){
-
-char name[14];
-char names[5][14] ={"Alaa","Osama","Mamdouh","Samy","Hossain"};
-
-puts("Enter your first name: ");
-gets(name);
-
-if(findName(names,5,name)==1)
-puts("Welcome");
-else
-puts("Goodby");
 }
